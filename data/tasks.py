d19b9182bd1b76bce4d817187bd5e4b1fe5245a6
qandrei01 <qandreicretu@gmail.com>
2023-04-18 20:24:49 +0300

    fourth test

---
+++ data/tasks.py
@@ -0,0 +1,23 @@
+import requests
+
+from data.get_url import ENDPOINT
+
+
+def create_task(payload):
+    return requests.put(ENDPOINT + "/create-task", json=payload)
+
+
+def get_task(task_id):
+    return requests.get(ENDPOINT + f"/get-task/{task_id}")
+
+
+def new_task_payload():
+    return {
+        "content": "test_content",
+        "user_id": "test_user_id",
+        "is_done": False,
+    }
+
+
+def update_task(payload):
+    return requests.put(ENDPOINT + "/update-task", json=payload)
---
+++ tests/test_update_task.py
@@ -0,0 +1,17 @@
+import requests
+
+from data.tasks import new_task_payload, create_task, update_task
+
+
+def test_update_task():
+    payload = new_task_payload()
+    create_task_response = create_task(payload)
+    task_id = create_task_response.json()["task"]["task_id"]
+    new_payload = {
+        "user_id": payload["user_id"],
+        "task_id": task_id,
+        "content": "test_updated_content",
+        "is_done": True,
+    }
+    update_task_response = update_task(new_payload)
+    assert update_task_response.status_code == 200
