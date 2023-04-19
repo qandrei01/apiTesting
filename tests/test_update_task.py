803a7ac54a264d2d6bcc01cbdb4f0edf5d4bb08d
qandrei01 <qandreicretu@gmail.com>
2023-04-19 19:55:35 +0300

    added more asserts to update task

--- tests/test_update_task.py
+++ tests/test_update_task.py
@@ -1,10 +1,11 @@
 
-from data.tasks import new_task_payload, create_task, update_task
+from data.tasks import new_task_payload, create_task, update_task, get_task
 
 
 def test_update_task():
     payload = new_task_payload()
     create_task_response = create_task(payload)
+    assert create_task_response.status_code == 200
     task_id = create_task_response.json()["task"]["task_id"]
     new_payload = {
         "user_id": payload["user_id"],
@@ -14,3 +15,9 @@ def test_update_task():
     }
     update_task_response = update_task(new_payload)
     assert update_task_response.status_code == 200
+
+    get_task_response = get_task(task_id)
+    assert get_task_response.status_code == 200
+    get_task_data = get_task_response.json()
+    assert get_task_data["content"] == new_payload["content"]
+    assert get_task_data["is_done"] == new_payload["is_done"]
