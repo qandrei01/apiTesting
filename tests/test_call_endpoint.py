fd9129a10445fc2518ea6f3920d833da6d70cf80
qandrei01 <qandreicretu@gmail.com>
2023-04-27 19:46:32 +0300

    add duration test example

--- tests/test_call_endpoint.py
+++ tests/test_call_endpoint.py
@@ -1,6 +1,18 @@
+
+from datetime import datetime, timedelta
+
 import requests
 
 
 def test_can_call_endpoint(base_url):
     response = requests.get(base_url, timeout=2)
     assert response.status_code == 200
+
+    get_response_date = response.headers.get("Date")
+    format_response_date_dt = datetime.strptime(get_response_date, "%a, %d %b %Y %H:%M:%S %Z")
+    response_date = format_response_date_dt.isoformat()
+    actual_date = datetime.utcnow().isoformat()
+    actual_date_dt = datetime.fromisoformat(actual_date)
+    response_date_dt = datetime.fromisoformat(response_date)
+    time_diff = abs(actual_date_dt - response_date_dt)
+    assert time_diff <= timedelta(seconds=1)
