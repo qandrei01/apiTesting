fcf9b2921b6b1b92ced0623b1562b2515d4d7a30
qandrei01 <qandreicretu@gmail.com>
2023-04-27 18:02:33 +0300

    add pytest html report

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,3 @@
-import socket
-import subprocess
 
 import pytest
 
@@ -22,19 +20,3 @@ def tasks():
 def assert_util():
     assert_util = AssertUtil
     return assert_util
-
-
-@pytest.hookimpl(trylast=True)
-def pytest_terminal_summary(terminalreporter):
-    try:
-        generate_report = subprocess.run(("allure", "generate", "-c"), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        generate_report.check_returncode()
-        hostname = socket.gethostname()
-        ip_address = socket.gethostbyname(hostname)
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('localhost', 0))
-            port = s.getsockname()[1]
-        report_url = f"http://{ip_address}:{port}/index.html"
-        terminalreporter.write_sep("-", f"Allure report: {report_url}")
-    except subprocess.CalledProcessError as e:
-        print(f"Check your setup for Allure. There was an error while generating report: \n {e}")
